6f9585deff96ef7a23625b738211fc1cca9d1f33
nev <gh@f-m.fm>
2022-05-16 14:15:18 -0700

    separate-docu-branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,10 +65,10 @@ const config = {
           {
             title: 'Docs',
             items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
+              // {
+              //   label: 'Tutorial',
+              //   to: '/docs/intro',
+              // },
             ],
           },
           {

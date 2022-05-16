48c09e520993c274fb676918f2672417567375d6
nev <gh@f-m.fm>
2022-05-16 15:20:16 -0700

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  trailingSlash:true,
   // organizationName: 'facebook', // Usually your GitHub org/user name.
   // projectName: 'docusaurus', // Usually your repo name.
 
